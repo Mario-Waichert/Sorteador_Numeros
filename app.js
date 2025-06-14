8fcd319919dd371766720848cd78510589052885
Mario Waichert <waichert.mario@gmail.com>
2025-06-14 13:06:01 +0100

    Garantir que o número sorteado seja exclusivo

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 function sortear() {
+    //lógica do botão sortear do index.html
     let quantidade = parseInt(document.getElementById('quantidade').value);
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
 
-    let sorteados = [];
+    let sorteados = [];//declaração da lista (array).
     let valor;
 
+    //loop para o user limitar a quantidade de números sorteados
     for (let i = 0; i < quantidade; i++) {
-        valor = numeroAleatorio(de,ate);
-        sorteados.push(valor);    
+          valor = numeroAleatorio(de,ate);
+          //loop para garantir que não haja repetição do número sorteado
+             while (sorteados.includes(valor)) {
+                 valor = numeroAleatorio(de,ate);
+       }
+        sorteados.push(valor);
     }
 
+    //exibindo na tela os números sorteados.
     let resultado = document.getElementById('resultado');
+    //propriedade que altera algo no html, neste caso exibir os números sorteados
     resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
     
-
 }
-
+//função para gerar o número aleatório
 function numeroAleatorio(min,max) {
-return Math.floor(Math.random() * (max - min + 1)) + min;
-    
+//o Math.floor arredonda o valor decimal do Math.random.
+//era possível utilizar o parseInt para arredondar.
+return Math.floor(Math.random() * (max - min + 1)) + min;    
 }
 
